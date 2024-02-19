b0052945bff167767fa4ed8d267c725d8416220a
Dmytro20777 <08dmutro11@gmail.com>
2024-02-19 11:36:10 +0200

    Homework6

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,16 +15,7 @@ const persistConfig = {
   storage,
 };
 
-const initialState = {
-  persons: {
-    contacts: {
-      items: [],
-      filters: {
-        name: ''
-      }
-    }
-  }
-};
+
 
 const rootReducer = combineReducers({
   persons: contactsReducer,
@@ -34,7 +25,6 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  preloadedState: initialState,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
@@ -43,11 +33,3 @@ export const store = configureStore({
     }),
 });
 export const persistor = persistStore(store);
-
-
-// [
-//         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
-//       ]
